008ec8720690e056fc433d4926a05f702b084b3c
Hyebin Lee <970905hyebin@gmail.com>
2023-03-28 14:46:29 +0900

    1 file added

--- src/weather.js
+++ src/weather.js
@@ -1,12 +1,15 @@
+import { API_KEY } from "../apikey";
+
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = "edb055b3894d8d0d763f5e8712a905aa";
+const KEY = API_KEY;
 
+console.log(KEY);
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   console.log("You live in", lat, lon);
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
